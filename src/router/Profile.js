255c89928eabb6ff77b7fcc3972e4abdd34ca2d0
marshall-ku <marshall@kakao.com>
2020-07-28 14:24:10 +0900

    update profile page - responsive image, update home page - add text shadow

--- src/router/Profile.js
+++ src/router/Profile.js
@@ -2,14 +2,20 @@ import React from "react";
 import "./Profile.css";
 
 export default function Profile() {
+    const ImgURL = `${process.env.PUBLIC_URL}/assets/images/IU`;
     return (
         <section id="profile">
-            <div
-                id="profileBG"
-                style={{
-                    backgroundImage: `url(${process.env.PUBLIC_URL}/assets/images/IU.jpg)`,
-                }}
-            ></div>
+            <picture id="profileBG">
+                <source
+                    media="(max-width: 1500px)"
+                    srcSet={`${ImgURL}-1500w.jpg`}
+                />
+                <source
+                    media="(max-width: 1920px)"
+                    srcSet={`${ImgURL}-1920w.jpg`}
+                />
+                <img src={`${ImgURL}.jpg`} alt="아이유" />
+            </picture>
             <div className="mainInfo">
                 <h1>아이유 / IU</h1>
                 <h2>이지은</h2>
