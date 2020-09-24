dd31693ef1e58fd57f111a8cabe4f2fa2944576b
marshall-ku <marshall@kakao.com>
2020-09-24 13:28:31 +0900

    shrink discography container width

--- src/router/Discography.js
+++ src/router/Discography.js
@@ -3,7 +3,7 @@ import List from "../components/List";
 
 export default function Discography() {
     return (
-        <section id="discography" className="list grid max-1400">
+        <section id="discography" className="list grid max-1060">
             <List
                 name="discography"
                 uri={`${process.env.PUBLIC_URL}/data/album.json`}
