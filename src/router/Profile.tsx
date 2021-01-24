6dc6fa7c77f7afd74af23261065b524a2dab0871
marshall-ku <marshall@kakao.com>
2021-01-24 13:03:54 +0900

    Update profile page

--- src/router/Profile.tsx
+++ src/router/Profile.tsx
@@ -1,20 +1,15 @@
-import React from "react";
 import "./Profile.css";
 
 export default function Profile() {
-    const ImgURL = `${process.env.PUBLIC_URL}/assets/images/IU`;
     return (
         <section id="profile">
             <picture id="profileBG">
-                <source
-                    media="(max-width: 1500px)"
-                    srcSet={`${ImgURL}-1500w.jpg`}
+                <img
+                    src="https://lh3.googleusercontent.com/pw/ACtC-3dOzFVbvP1FzkNPjGqwYZp3GUXJkpQ1_ceW6QQza0O-T-I618M95Il5WewStBJ3qzSd4ZS0e4ozXNq9cBH7DE1-I9DsVMYXuOBpZ4Ay9rZPcaH-pC7ZcIjYaeTmn284tIgeR-U8coqYr4Wv_khraxP7=w1920"
+                    srcSet="https://lh3.googleusercontent.com/pw/ACtC-3dOzFVbvP1FzkNPjGqwYZp3GUXJkpQ1_ceW6QQza0O-T-I618M95Il5WewStBJ3qzSd4ZS0e4ozXNq9cBH7DE1-I9DsVMYXuOBpZ4Ay9rZPcaH-pC7ZcIjYaeTmn284tIgeR-U8coqYr4Wv_khraxP7=w1920 1920w, https://lh3.googleusercontent.com/pw/ACtC-3dOzFVbvP1FzkNPjGqwYZp3GUXJkpQ1_ceW6QQza0O-T-I618M95Il5WewStBJ3qzSd4ZS0e4ozXNq9cBH7DE1-I9DsVMYXuOBpZ4Ay9rZPcaH-pC7ZcIjYaeTmn284tIgeR-U8coqYr4Wv_khraxP7=w600 600w, https://lh3.googleusercontent.com/pw/ACtC-3dOzFVbvP1FzkNPjGqwYZp3GUXJkpQ1_ceW6QQza0O-T-I618M95Il5WewStBJ3qzSd4ZS0e4ozXNq9cBH7DE1-I9DsVMYXuOBpZ4Ay9rZPcaH-pC7ZcIjYaeTmn284tIgeR-U8coqYr4Wv_khraxP7=w400 400w, https://lh3.googleusercontent.com/pw/ACtC-3dOzFVbvP1FzkNPjGqwYZp3GUXJkpQ1_ceW6QQza0O-T-I618M95Il5WewStBJ3qzSd4ZS0e4ozXNq9cBH7DE1-I9DsVMYXuOBpZ4Ay9rZPcaH-pC7ZcIjYaeTmn284tIgeR-U8coqYr4Wv_khraxP7=w1536 1536w, https://lh3.googleusercontent.com/pw/ACtC-3dOzFVbvP1FzkNPjGqwYZp3GUXJkpQ1_ceW6QQza0O-T-I618M95Il5WewStBJ3qzSd4ZS0e4ozXNq9cBH7DE1-I9DsVMYXuOBpZ4Ay9rZPcaH-pC7ZcIjYaeTmn284tIgeR-U8coqYr4Wv_khraxP7=w2048 2048w"
+                    sizes="(max-width: 1920px) 100vw, 1920px"
+                    alt="IU Celebrity"
                 />
-                <source
-                    media="(max-width: 1920px)"
-                    srcSet={`${ImgURL}-1920w.jpg`}
-                />
-                <img src={`${ImgURL}.jpg`} alt="아이유" />
             </picture>
             <div className="mainInfo">
                 <h1>아이유 / IU</h1>
