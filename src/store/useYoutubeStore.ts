4f7c4c9f05ce3d93337b3f025b870362f8a9606f
marshallku <marshall@kakao.com>
2022-03-27 20:57:05 +0900

    Add youtube video list data json file

--- src/store/useYoutubeStore.ts
+++ src/store/useYoutubeStore.ts
@@ -3,47 +3,11 @@ import create from "zustand";
 const useYoutubeStore = create<YoutubeStore>((set) => ({
     fetchList: async () => {
         const response = await fetch(
-            "/get?uri=https://www.youtube.com/c/dlwlrma/videos"
+            `${import.meta.env.BASE_URL}/data/youtube.json`
         );
-        const text = await response.text();
-        const parsed = JSON.parse(
-            text
-                .slice(text.indexOf('"tabs"'), text.indexOf('"header') - 3)
-                .replace('"tabs":', "")
-        );
-        const ytList =
-            parsed[1].tabRenderer.content.sectionListRenderer.contents[0]
-                .itemSectionRenderer.contents[0].gridRenderer.items;
+        const json = await response.json();
 
-        set((state) => ({
-            ...state,
-            data: ytList
-                .filter(
-                    (x: { gridVideoRenderer: any }) => !!x.gridVideoRenderer
-                )
-                .map(
-                    ({ gridVideoRenderer }: { gridVideoRenderer: any }) =>
-                        gridVideoRenderer
-                )
-                .map(
-                    ({
-                        title,
-                        videoId,
-                        viewCountText,
-                        publishedTimeText,
-                    }: {
-                        title: any;
-                        videoId: any;
-                        viewCountText: any;
-                        publishedTimeText: any;
-                    }) => ({
-                        title: `${title.runs[0].text}`,
-                        videoId: `${videoId}`,
-                        viewCount: `${viewCountText.simpleText}`,
-                        publishedAt: `${publishedTimeText.simpleText}`,
-                    })
-                ),
-        }));
+        set((state) => ({ ...state, data: json }));
     },
 }));
 
