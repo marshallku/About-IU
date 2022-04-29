45d42c97e4bf67b5b396bd864a6cfe720df51b32
marshall-ku <marshall@kakao.com>
2022-04-29 23:50:06 +0900

    Do exit script if curl fails

--- .github/scripts/fetch-youtube.sh
+++ .github/scripts/fetch-youtube.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-response=$(curl "https://www.youtube.com/c/dlwlrma/videos")
+response=$(curl "https://www.youtube.com/c/dlwlrma/videos")|| exit 1
 
 echo $response > tmp.txt
 node .github/scripts/parse-data.js > public/data/youtube.json
