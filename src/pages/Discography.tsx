f00069233c8a565e4b04213fb904613d08e4e495
marshall-ku <marshall@kakao.com>
2022-06-16 00:31:47 +0900

    Remove useless literals

--- src/pages/Discography.tsx
+++ src/pages/Discography.tsx
@@ -48,7 +48,7 @@ export default function Discography() {
                             style={{
                                 backgroundImage: `url("${
                                     import.meta.env.BASE_URL
-                                }${`/assets/images/album_cover/${name}.jpg`}")`,
+                                }/assets/images/album_cover/${name}.jpg")`,
                             }}
                         />
                         <header className="grid-item__content">
