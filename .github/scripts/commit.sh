9c7d7f20610b327a4b7100307e0c46e776e8d0b4
Marshall Ku <marshall@kakao.com>
2023-06-04 17:44:24 +0900

    Fix committer in workflow

--- .github/scripts/commit.sh
+++ .github/scripts/commit.sh
@@ -8,7 +8,8 @@ github_env="$1"
 
 echo 'status=TRUE' >>"$github_env"
 
-git config --global user.name github-actions
+git config user.name github-actions[bot]
+git config user.email 41898282+github-actions[bot]@users.noreply.github.com
 git remote set-url origin "https://x-access-token:$TOKEN@github.com/$REPOSITORY"
 git add -A
 git commit -m "$MESSAGE"
