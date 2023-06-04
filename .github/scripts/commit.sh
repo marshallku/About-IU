3d9e7d047cd5a657a576952321714a401b1835c1
Marshall Ku <marshall@kakao.com>
2023-06-04 17:31:45 +0900

    Simplify commit script

--- .github/scripts/commit.sh
+++ .github/scripts/commit.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-if [[ `git status --porcelain` ]]; then
-    git config --global user.name github-actions
-    git remote set-url origin https://x-access-token:$TOKEN@github.com/$REPOSITORY
-    git add -A
-    git commit -m "$MESSAGE"
-    git push
+if [[ -z $(git status --porcelain) ]]; then
+    exit 0
 fi
+
+git config --global user.name github-actions
+git remote set-url origin https://x-access-token:$TOKEN@github.com/$REPOSITORY
+git add -A
+git commit -m "$MESSAGE"
+git push
