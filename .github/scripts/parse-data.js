cb755e47394f66e63454b1e37eada4fa9eb983c5
Marshall Ku <marshall@kakao.com>
2024-07-09 08:27:42 +0900

    Improve stability of data parser

--- .github/scripts/parse-data.js
+++ .github/scripts/parse-data.js
@@ -1,22 +1,65 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require("node:fs");
+const path = require("node:path");
+const { JSDOM } = require("jsdom");
 
 const data = fs.readFileSync(path.resolve(__dirname, "../../tmp.txt"), "utf8");
-const parsed = JSON.parse(
-    data
-        .slice(
-            data.indexOf('"tabs"'),
-            data.lastIndexOf('"header":{"c4TabbedHeaderRenderer"') - 3
-        )
-        .replace('"tabs":', "")
-);
-const list = parsed[1].tabRenderer.content.richGridRenderer.contents;
+const {
+    window: { document },
+} = new JSDOM(data);
+
+const targetScript = [...document.querySelectorAll("script")]
+    .map((x) => x.innerHTML)
+    .find((x) => x.includes("ytInitialData") && x.includes("i.ytimg.com"));
+
+if (!targetScript) {
+    return;
+}
+
+const tryParseYoutubeData = (data) => {
+    const vm = require("vm");
+
+    const context = {};
+    vm.createContext(context);
+
+    const scriptContent = `
+    (function() {
+      ${data}
+      if (typeof ytInitialData !== 'undefined') {
+        return ytInitialData;
+      }
+    })()
+  `;
+
+    try {
+        const script = new vm.Script(scriptContent);
+        const ytInitialData = script.runInContext(context);
+
+        return ytInitialData;
+    } catch (error) {
+        return null;
+    }
+};
+
+const parsedData = tryParseYoutubeData(targetScript);
+const list = parsedData?.contents?.twoColumnBrowseResultsRenderer?.tabs?.find(
+    (x) => typeof x === "object" && x.tabRenderer?.content
+).tabRenderer.content?.richGridRenderer?.contents;
+
+if (!list || !Array.isArray(list)) {
+    return;
+}
+
 const parsedList = list
-    .filter((x) => !!x.richItemRenderer)
-    .map(({ richItemRenderer }) => richItemRenderer.content.videoRenderer)
+    .filter((x) => !!x?.richItemRenderer)
+    .map(({ richItemRenderer }) => richItemRenderer?.content?.videoRenderer)
+    .filter((x) => !!x && typeof x === "object" && x.title && x.videoId)
     .map(({ title, videoId }) => ({
         title: `${title.runs[0].text}`,
         videoId: `${videoId}`,
     }));
+
+if (!parsedList.length) {
+    return;
+}
 
 process.stdout.write(JSON.stringify(parsedList));
